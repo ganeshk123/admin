22f677aac70e07c9a478655c874661e3555b7aee
ganeshk123 <ganesh.korde7@gmail.com>
2022-07-15 02:01:56 +0530

    Added my project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Grocery Delivery App
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2021-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseUrl: 'https://api.csuk.uk/public/api/', // ex https://api.csuk.uk/public/api/ don't forgot to add public/api/ at the end
+  imageUrl: 'https://api.csuk.uk/public/storage/images/', // ex https://api.csuk.uk/public/storage/images/ don't forgot to add public/storage/images at the end
+};
